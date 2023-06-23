5029071f60f129f15839c910f845b11e0e3f1d9a
MohamedTwahir <mohamedtwahir977@gmail.com>
2023-06-23 12:14:27 -0500

    my_simple_calculator

---
+++ README.md
@@ -0,0 +1,9 @@
+I will be building a simple calculator in my path to learning python through project series.
+# This is my day two on that series: i have currently learnt about:
+1.How to comment in python using "#"
+2.Standard input and output in python, input() and print()
+3. Variables in python
+4. data types in python
+
+My focus is not creating an actual application but rather gaining knowledge on concepts in python i might come back and improve some functionality later on when am experienced enough:)
+Am also trying to understand how python can be of us to our daily arithmetic operations.
---
+++ simplecalc.py
@@ -0,0 +1,20 @@
+# Building a simple Calculator with python
+first_number = float(input('Please Enter a number: '))
+second_number = float(input('Please Enter another number to be calculated with: '))
+
+
+# Addition
+print(f" Result for addition is {first_number + second_number}")
+
+#Subtraction
+print(f"Result for subtraction is {first_number - second_number}")
+
+#Multiplication
+print(f"Result for multiplication is {first_number * second_number}")
+
+# Exponential 
+print(f"Result for exponential is {first_number ** second_number}")
+
+
+#Message
+print("Thank you hope i have helped as your able friend in mathematics")
